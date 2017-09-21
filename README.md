412612885bc70c39272bff362238456764ef0ce0
Jan Kaifer <kaifer741@gmail.com>
2017-09-21 23:45:29 +0200

    ready for pypi?

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Numerishy
+[Numerishy]
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'numerishy',
+  packages = ['numerishy'], # this must be the same as the name above
+  version = '1.0',
+  description = 'Strongest numbers survive. ',
+  author = 'Jan Kaifer',
+  author_email = 'kaifer741@gmail.com',
+  url = 'https://github.com/jankaifer/numerishy', # use the URL to the github repo
+  download_url = 'https://github.com/jankaifer/numerishy/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['game', 'curses', 'best', 'must'], # arbitrary keywords
+  classifiers = [],
+)
