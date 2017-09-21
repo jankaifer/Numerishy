b3fff3ef781d246cb416e81b647500a07766f5e0
Jan Kaifer <kaifer741@gmail.com>
2017-09-22 01:11:05 +0200

    v1.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'numerishy',
   scripts = ['numerishy'],
-  version = '1.1',
+  version = '1.2',
   description = 'Strongest numbers survive. ',
   author = 'Jan Kaifer',
   author_email = 'kaifer741@gmail.com',
   url = 'https://github.com/jankaifer/numerishy',
-  download_url = 'https://github.com/JanKaifer/Numerishy/archive/v1.1.tar.gz',
+  download_url = 'https://github.com/JanKaifer/Numerishy/archive/v1.2.tar.gz',
   keywords = ['game', 'curses', 'best', 'must'],
   classifiers = [],
 )
