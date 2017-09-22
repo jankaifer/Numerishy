78e1856864b3b8b3631c28cb05e7f38800836a3b
Jan Kaifer <kaifer741@gmail.com>
2017-09-22 02:06:30 +0200

    v1.5
    clearing buffer

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'numerishy',
   scripts = ['numerishy'],
-  version = '1.4',
+  version = '1.5',
   description = 'Strongest numbers survive. ',
   author = 'Jan Kaifer',
   author_email = 'kaifer741@gmail.com',
   url = 'https://github.com/jankaifer/numerishy',
-  download_url = 'https://github.com/JanKaifer/Numerishy/archive/v1.4.tar.gz',
+  download_url = 'https://github.com/JanKaifer/Numerishy/archive/v1.5.tar.gz',
   keywords = ['game', 'curses', 'best', 'must'],
   classifiers = [],
 )
