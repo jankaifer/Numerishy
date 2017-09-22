9976e223b6e7f41c47ff9e46fd798780d55e6b31
Jan Kaifer <kaifer741@gmail.com>
2017-09-22 02:51:49 +0200

    v1.6
    added minimal speed foor Numbers

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'numerishy',
   scripts = ['numerishy'],
-  version = '1.5',
+  version = '1.6',
   description = 'Strongest numbers survive. ',
   author = 'Jan Kaifer',
   author_email = 'kaifer741@gmail.com',
   url = 'https://github.com/jankaifer/numerishy',
-  download_url = 'https://github.com/JanKaifer/Numerishy/archive/v1.5.tar.gz',
+  download_url = 'https://github.com/JanKaifer/Numerishy/archive/v1.6.tar.gz',
   keywords = ['game', 'curses', 'best', 'must'],
   classifiers = [],
 )
