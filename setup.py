a5ee27f7ee2ffa56831e8031d80b9b42906c42d1
Jan Kaifer <kaifer741@gmail.com>
2017-11-01 18:11:05 +0100

    updated enemy generator

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'numerishy',
   scripts = ['numerishy', 'physics.py', 'graphic.py'],
-  version = '2.0',
-  description = 'Strongest numbers survive. ',
+  version = '2.2',
+  description = 'Strongest numbers survive. - YEAH Special version with titles',
   author = 'Jan Kaifer',
   author_email = 'kaifer741@gmail.com',
   url = 'https://github.com/jankaifer/numerishy',
-  download_url = 'https://github.com/JanKaifer/Numerishy/archive/v2.0.tar.gz',
+  download_url = 'https://github.com/JanKaifer/Numerishy/archive/v2.2.tar.gz',
   keywords = ['game', 'curses', 'best', 'must'],
   classifiers = [],
 )
